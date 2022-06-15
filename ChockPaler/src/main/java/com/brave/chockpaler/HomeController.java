7752a9e200f405ac42846baefa6326d294f2b963
acorn <acorn@DESKTOP-FRU0154>
2022-06-15 16:54:43 +0900

    Setting commit

--- ChockPaler/src/main/java/com/brave/chockpaler/HomeController.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/HomeController.java
@@ -1,37 +1,23 @@
 package com.brave.chockpaler;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping("/home")
+	public String home(HttpServletRequest request) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		List<String> list=new ArrayList<String>();
+		list.add("촠팔러");
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		request.setAttribute("list", list);
 		
 		return "home";
 	}
--- ChockPaler/src/main/webapp/WEB-INF/views/home.jsp
+++ ChockPaler/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,13 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>   
 <html>
 <head>
-	<title>Home</title>
+	<title>/home.jsp</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+<h1> 프로젝트 페이지 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+	
 </body>
 </html>
