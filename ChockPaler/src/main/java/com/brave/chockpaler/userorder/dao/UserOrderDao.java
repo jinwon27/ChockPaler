885fe38146017891d5c4722780988ca8b9215797
leekisaung <kistar0621@naver.com>
2022-06-16 16:43:24 +0900

    UserOrder_

--- ChockPaler/src/main/java/com/brave/chockpaler/userorder/dao/UserOrderDao.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/userorder/dao/UserOrderDao.java
@@ -1,5 +1,13 @@
 package com.brave.chockpaler.userorder.dao;
 
-public interface UserOrderDao {
+import java.util.List;
 
+import com.brave.chockpaler.userorder.dto.UserOrderDto;
+
+public interface UserOrderDao {
+	public void insert(UserOrderDto dto);
+	public UserOrderDto getData(int num);
+	public void delete(int num);
+	public List<UserOrderDto> getList(UserOrderDto dto);
+	public int getCount(UserOrderDto dto);
 }
--- ChockPaler/src/main/java/com/brave/chockpaler/userorder/dto/UserOrderDto.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/userorder/dto/UserOrderDto.java
@@ -1,5 +1,58 @@
 package com.brave.chockpaler.userorder.dto;
 
 public class UserOrderDto {
+	private int num;;
+	private String id;
+	private int Item_Code;
+	private String addr;
+	
+	public UserOrderDto() {}
+	
+	
+	
 
+	public UserOrderDto(int num, String id, int item_Code, String addr) {
+		super();
+		this.num = num;
+		this.id = id;
+		Item_Code = item_Code;
+		this.addr = addr;
+	}
+
+
+
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getItem_Code() {
+		return Item_Code;
+	}
+
+	public void setItem_Code(int item_Code) {
+		Item_Code = item_Code;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
 }
