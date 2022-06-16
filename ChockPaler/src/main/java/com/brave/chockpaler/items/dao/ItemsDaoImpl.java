56d65db1eac4cc5292de3cf9a6c80505e1dff1f8
chaeyounghwa <chaeyh0417@naver.com>
2022-06-16 16:49:24 +0900

    itemsDaImpl first

--- ChockPaler/src/main/java/com/brave/chockpaler/items/dao/ItemsDaoImpl.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/items/dao/ItemsDaoImpl.java
@@ -1,5 +1,51 @@
 package com.brave.chockpaler.items.dao;
 
-public class ItemsDaoImpl {
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.brave.chockpaler.items.dto.ItemsDto;
+
+@Repository
+public class ItemsDaoImpl implements ItemsDao {
+
+	private SqlSession session;
+	
+	@Override
+	public List<ItemsDto> getList(ItemsDto dto) {
+		
+		return null;
+	}
+
+	@Override
+	public void insert(ItemsDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(ItemsDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ItemsDto getData(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemsDto SalCount(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
