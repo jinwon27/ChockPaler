c903ba5598b73e68bd7d18eaee0082b21b54a339
eusang <dbtkd98@naver.com>
2022-06-17 12:41:39 +0900

    회원가입, 로그인폼

--- ChockPaler/src/main/java/com/brave/chockpaler/users/controller/UsersController.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/users/controller/UsersController.java
@@ -1,5 +1,78 @@
 package com.brave.chockpaler.users.controller;
 
-public class UsersController {
+import java.net.URLEncoder;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.brave.chockpaler.users.dto.UsersDto;
+import com.brave.chockpaler.users.service.UsersService;
+
+
+@Controller
+public class UsersController {
+	
+	@Autowired
+	private UsersService service;
+	
+	@RequestMapping(value = "/users/signupform", method = RequestMethod.GET)
+	public String signupForm() {
+		
+		return "users/signupform";
+	}
+	
+	@RequestMapping(value = "/users/signup", method = RequestMethod.POST)
+	public ModelAndView signup(ModelAndView mView, UsersDto dto) {
+		
+		service.addUser(dto);
+		
+		mView.setViewName("users/signup");
+		return mView;
+	}
+	
+	@RequestMapping("/users/loginform")
+	public String loginform() {
+		
+		return "users/loginform";
+	}
+	
+	@RequestMapping("/users/login")
+	public ModelAndView login(ModelAndView mView, UsersDto dto,
+			@RequestParam String url, HttpSession session) {
+		
+		service.loginProcess(dto, session);
+		
+		String encodedUrl=URLEncoder.encode(url);
+		mView.addObject("url", url);
+		mView.addObject("encodedUrl", encodedUrl);
+		
+		mView.setViewName("users/login");
+		return mView;
+	}
+	
+	@RequestMapping("/users/info")
+	public ModelAndView authInfo(HttpSession session, ModelAndView mView, 
+			HttpServletRequest request) {
+		
+		service.getInfo(session, mView);
+		
+		mView.setViewName("users/info");
+		return mView;
+	}
+	
+	@RequestMapping("/users/checkid")
+	@ResponseBody
+	public Map<String, Object> checkid(@RequestParam String inputId){
+		//UsersService 가 리턴해주는 Map 을 리턴해서 json 문자열을 응답한다. 
+		return service.isExistId(inputId);
+	}
 }
--- ChockPaler/src/main/java/com/brave/chockpaler/users/service/UsersServiceImpl.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/users/service/UsersServiceImpl.java
@@ -1,5 +1,95 @@
 package com.brave.chockpaler.users.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.brave.chockpaler.users.dao.UsersDao;
+import com.brave.chockpaler.users.dto.UsersDto;
+
+@Service
 public class UsersServiceImpl implements UsersService{
+
+	@Autowired
+	private UsersDao dao;
+	
+	@Override
+	public Map<String, Object> isExistId(String inputId) {
+		
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("isExist", dao.isExist(inputId));
+		return map;
+			
+	}
+
+	@Override
+	public void addUser(UsersDto dto) {
+		//사용자가 입력한 비밀 번호를 읽어와서 
+		String pwd=dto.getPwd();
+		//암호화 한 후에 
+		BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+		String encodedPwd=encoder.encode(pwd);
+		//dto 에 다시 넣어준다.
+		dto.setPwd(encodedPwd);
+		
+		dao.insert(dto);
+	}
+
+	@Override
+	public void loginProcess(UsersDto dto, HttpSession session) {
+		boolean isValid=false;
+		
+		UsersDto result=dao.getData(dto.getId());
+		if(result != null) {
+			String encodedPwd=result.getPwd();
+			String inputPwd=dto.getPwd();
+			isValid=BCrypt.checkpw(inputPwd, encodedPwd);
+		}
+		
+		if(isValid) {
+			session.setAttribute("id", dto.getId());
+		}
+		
+	}
+
+	@Override
+	public void getInfo(HttpSession session, ModelAndView mView) {
+		String id=(String)session.getAttribute("id");
+		UsersDto dto=dao.getData(id);
+		mView.addObject("dto", dto);
+	}
+
+	@Override
+	public void updateUserPwd(HttpSession session, UsersDto dto, ModelAndView mView) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Object> saveProfileImage(HttpServletRequest request, MultipartFile mFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateUser(UsersDto dto, HttpSession session) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(HttpSession session, ModelAndView mView) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
