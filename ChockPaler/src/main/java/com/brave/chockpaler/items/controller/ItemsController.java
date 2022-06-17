c04c94d176490628c860f6eff17ff549b33dfb49
acorn <acorn@DESKTOP-8GULLHG>
2022-06-17 15:09:42 +0900

    hello yh

--- ChockPaler/src/main/java/com/brave/chockpaler/items/controller/ItemsController.java
+++ ChockPaler/src/main/java/com/brave/chockpaler/items/controller/ItemsController.java
@@ -1,5 +1,7 @@
 package com.brave.chockpaler.items.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +13,18 @@ import com.brave.chockpaler.items.service.ItemsService;
 @Controller
 public class ItemsController {
 	
-	@Autowired
-	private ItemsService service;
+	//@Autowired
+	//private ItemsService service;
 	
 	@RequestMapping("/items/uploadform")
-	public String uploadForm() {
-		return "items/uploadform.jsp";
+	public String uploadForm(HttpServletRequest request) {
+		return "items/uploadform";
 	}
 	
 	@RequestMapping("/items/upload.do")
 	public String upload(@RequestParam ItemsDto dto) {
 		// 폼으로 전송된 데이터를 디비에 인설트(저장) 하기
-		service.saveItems(dto);
-		return "items/upload.jsp";
+		//service.saveItems(dto);
+		return "items/upload";
 	}
 }
